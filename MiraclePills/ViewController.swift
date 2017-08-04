0cc3360c93a4e45a45ff8610821f7ea740857889
Darryl Lim <dlimusa2002@gmail.com>
2017-08-04 07:49:32 -0600

    Added textfields, labels, and UIPickerview
    Added textfields, labels, and a UIPickerview which all work.

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var zipcodeTextField: UITextField!
+    @IBOutlet weak var zipcodeLabel: UILabel!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+  
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +32,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //Does something when the state button is pressed
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+        countryLabel.isHidden = true
+        countryTextField.isHidden = true
+        zipcodeLabel.isHidden = true
+        zipcodeTextField.isHidden = true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        countryLabel.isHidden = false
+        countryTextField.isHidden = false
+        zipcodeLabel.isHidden = false
+        zipcodeTextField.isHidden = false
+    }
+    
+    
+   }
 
-}
 
